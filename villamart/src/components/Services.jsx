4a899b9f93c928a4b45c813d08b8f46e477edfaf
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-03-19 15:53:14 +0100

    this commit is building the service section of villamart

--- villamart/src/components/Services.jsx
+++ villamart/src/components/Services.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-
+import "../App.css";
 export default function Services() {
   return (
-    <main className="max-w-7xl my-[2.5rem] mx-auto">
-      <h1 className="text-[3rem] text-bold font-[Sora] text-center text-[#c40414]">
+    <main className="max-w-7xl mt-[3.5rem] mx-auto">
+      <h1 className="text-[3rem] mb-[2.5rem] text-bold font-[Sora] text-center text-[#c40414]">
         {" "}
         Our Services
       </h1>
       <section className="w-full flex justify-between items-center h-[70vh] ">
-        <div className="w-[49%] h-full">
-          <div className="w-full h-[65%] border border-red-500 rounded "></div>
+        <div className="w-[49%] h-full  shadow">
+          <div className="w-full h-[65%] rounded-[0.45rem]  "></div>
         </div>
         <div className="w-[49%] h-full flex flex-col justify-between items-center">
-          <div className=" w-full h-[30%] border border-red-500 rounded"></div>
-          <div className=" w-full h-[30%] border border-red-500 rounded"></div>
-          <div className=" w-full h-[30%] border border-red-500 rounded"></div>
+          <div className=" w-full h-[30%] rounded-[0.45rem] shadow"></div>
+          <div className=" w-full h-[30%] rounded-[0.45rem]  shadow"></div>
+          <div className=" w-full h-[30%] rounded-[0.45rem]  shadow"></div>
         </div>
       </section>
     </main>
